6dba335883fb0a26c544ff1bb0b3b6de4e8d29f8
MimFSD <mimislam878@gmail.com>
2024-07-12 00:25:03 +0600

    Only pending assignment

--- src/components/PendingAssignment/PendingAssignment.jsx
+++ src/components/PendingAssignment/PendingAssignment.jsx
@@ -4,22 +4,22 @@ import OnlyPendingAssignment from "./OnlyPendingAssignment";
 import { AuthProvider } from "../../AuthProvider/AuthContext";
 import { Helmet } from "react-helmet-async";
 
-const PendingAssignment = () => {
+const PendingAssignment = () => {                   
     const { user, theme } = useContext(AuthProvider);
     const [pendingAssignment, setpendingAssignment] = useState([]);
     useEffect(() => {
         fetch(`https://online-group-study-server-site.vercel.app/submitAssignment?email=${user?.email}`, { credentials: 'include' })
             .then(res => res.json())
-            .then(data => {
+            .then(data => {                 
                 setpendingAssignment(data)
-            })
+            })                              
     }, [])
     return (
         <div className={theme === 'light' ? "bg-gray-100 min-h-screen" : "bg-gray-700 min-h-screen"}>
             <Helmet>
                 <title>E-Group Study - PendingAssignment</title>
             </Helmet>
-            {
+            {              
                 pendingAssignment.map(data => <OnlyPendingAssignment key={data._id} data={data}></OnlyPendingAssignment>)
             }
         </div>
