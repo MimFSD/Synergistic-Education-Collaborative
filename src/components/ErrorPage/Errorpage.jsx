cba9ed6c7fd6dd35f4abd159a4d031b65292e097
MimFSD <mimislam878@gmail.com>
2024-07-12 00:17:12 +0600

    Error page

--- src/components/ErrorPage/Errorpage.jsx
+++ src/components/ErrorPage/Errorpage.jsx
@@ -3,14 +3,14 @@ import { NavLink } from "react-router-dom";
 
 const Errorpage = () => {
     return (
-        <div>
+        <div>       
             <Helmet>
                 <title>E-Group Study - ErrorPage</title>
             </Helmet>
             <div className="border w-80 bg-green-200 rounded-lg p-10 text-center ml-[30rem] my-[6rem]">
                 <h2 className="text-2xl font-bold text-red-600">Opps !! This page is not found</h2>
                 <butto className='btn mt-3 font-semibold'><NavLink to='/'>Back to Home</NavLink></butto>
-            </div>
+            </div> 
         </div>
     );
 };
