937c385ccd5233a841254c2ef38298cbd9ed04b1
MimFSD <mimislam878@gmail.com>
2024-07-12 00:20:23 +0600

    banner section added

--- src/components/Pages/Home/Banner.jsx
+++ src/components/Pages/Home/Banner.jsx
@@ -14,7 +14,7 @@ const Banner = () => {
                     <div className='relative md:left-[120px] lg:left-[620px] mt-5'>
                         <img className='rounded-full h-32 md:h-40 lg:h-48 w-32 md:w-40 lg:w-48 border-4 shadow-2xl' src={bannerImage} alt="" />
                         <img className='rounded-full h-32 md:h-40 lg:h-48 w-32 md:w-40 lg:w-48 border-4 mt-16 shadow-2xl' src={bannerImage2} alt="" />
-                    </div>
+                    </div>                  
                     <img className='relative rounded-full h-32 md:h-40 lg:h-48 w-32 md:w-40 lg:w-48 border-4 md:left-[180px] lg:left-[655px] mt-28 lg:mt-36 shadow-2xl' src={bannerImage3} alt="" />
                     <div className='relative md:left-[230px] lg:left-[680px] mt-5'>
                         <img className='rounded-full h-32 md:h-40 lg:h-48 w-32 md:w-40 lg:w-48 border-4 shadow-2xl' src={bannerImage4} alt="" />
